4e793fd68426440207d6deeb13ebfe29a46b5425
Alex Stocks <stocksalex@icloud.com>
2020-09-21 20:40:37 +0100

    Detect Sandbox finished

--- modules/DetectSandbox.py
+++ modules/DetectSandbox.py
@@ -3,102 +3,101 @@ import random
 import time
 import sys
 
-user32 = ctypes.windll.user32
-kernel32 = ctypes.windll.kernerl32
+windows32User = ctypes.windll.user32
+windows32Kernel = ctypes.windll.kernerl32
 
-keystrokes = 0
-mouse_clicks = 0
-double_clicks = 0
-
-class LastInputInfo(ctypes.Structure):
-    _fields_ = [("cbsize", ctypes.c_unit),
-                ("dwTime", ctypes.c_ulong)
-                ]
+numberOfKeystrokes = 0
+numberOfMouseClicks = 0
+numberOfDoubleClicks = 0
 
 
-def get_last_input():
-    struct_lastinputinfo = LastInputInfo()
-    struct_lastinputinfo.cbsize = ctypes.sizeof(LastInputInfo)
+class LastInput(ctypes.Structure):
+    _fields_ = [("cbsize", ctypes.c_unit), ("dwTime", ctypes.c_ulong)]
 
-    user32.GetLastInputInfo(ctypes.byref(struct_lastinputinfo))
 
-    run_time = kernel32.GetTickCount()
+def getLastInput():
+    LastInputStruct = LastInput()
+    LastInputStruct.cbsize = ctypes.sizeof(LastInput)
 
-    elapsed = run_time - struct_lastinputinfo.dwTime
+    windows32User.GetLastInput(ctypes.byref(LastInputStruct))
 
-    print("[*] %d milliseconds since the last input event." % elapsed)
+    runTime = windows32Kernel.GetTickCount()
 
-    return elapsed
+    timeElapsed = runTime - LastInputStruct.dwTime
 
-def get_key_press():
+    print("[*] %d milliseconds since the last input event." % timeElapsed)
 
-    global mouse_clicks
-    global keystrokes
+    return timeElapsed
+
+
+def getKeyPress():
+    global numberOfMouseClicks
+    global numberOfKeystrokes
 
     for i in range(0, 0xff):
-        if user32.GetAsyncKeyState(i) == -32767:
+        if windows32User.GetAsyncKeyState(i) == -32767:
 
             # if mouse click
             if i == 0x1:
-                mouse_clicks += 1
+                numberOfMouseClicks += 1
                 return time.time()
             elif 127 > i > 32:
-                keystrokes += 1
+                numberOfKeystrokes += 1
 
     return None
 
-def detect_sandbox():
 
-    global mouse_clicks
-    global keystrokes
+def detectSandbox():
+    global numberOfMouseClicks
+    global numberOfKeystrokes
 
-    max_keystrokes = random.randint(10, 25)
-    max_mouse_clicks = random.randint(5, 25)
+    max_numberOfKeystrokes = random.randint(10, 25)
+    max_numberOfMouseClicks = random.randint(5, 25)
 
-    double_clicks = 0
-    max_double_clicks = 10
-    double_click_threshold = 0.250
-    first_double_click = None
+    numberOfDoubleClicks = 0
+    maxNumberOfDoubleClicks = 10
+    doubleClickThreshold = 0.250
+    firstDoubleClick = None
 
-    average_mousetime = 0
-    max_input_threshold = 30000
+    maxInputThreshold = 30000
 
-    previous_timestamp = None
-    detection_complete = False
+    previousTimestamp = None
+    finishedDetection = False
 
-    last_input = get_last_input()
+    lastInput = getLastInput()
 
-    if last_input >= max_input_threshold:
+    if lastInput >= maxInputThreshold:
         sys.exit(0)
 
-    while not detection_complete:
+    while not finishedDetection:
 
-        # check for keypresses or mouse-clicks, if value returned then it is the timestamp of when it ocurred
-        keypress_time = get_key_press()
+        # check for key-presses or mouse-clicks, if value returned then it is the timestamp of when it ocurred
+        timeKeyHeldFor = getKeyPress()
 
-        if keypress_time is not None and previous_timestamp is not None:
+        if timeKeyHeldFor is not None and previousTimestamp is not None:
 
-            elapsed = keypress_time - previous_timestamp
+            timeElapsed = timeKeyHeldFor - previousTimestamp
 
-            if elapsed <= double_click_threshold:
-                double_clicks += 1
+            if timeElapsed <= doubleClickThreshold:
+                numberOfDoubleClicks += 1
 
-                if first_double_click is None:
-                    first_double_click = time.time()
+                if firstDoubleClick is None:
+                    firstDoubleClick = time.time()
 
                 else:
                     # seeing if trying to fake out snadbox detction techniques
-                    if double_clicks == max_double_clicks:
-                        if keypress_time - first_double_click <= (max_double_clicks * double_click_threshold):
+                    if numberOfDoubleClicks == maxNumberOfDoubleClicks:
+                        if timeKeyHeldFor - firstDoubleClick <= (maxNumberOfDoubleClicks * doubleClickThreshold):
                             sys.exit(0)
 
-            if keystrokes >= max_keystrokes and double_clicks >= max_double_clicks and mouse_clicks >= max_mouse_clicks:
+            if numberOfKeystrokes >= max_numberOfKeystrokes and numberOfDoubleClicks >= maxNumberOfDoubleClicks and numberOfMouseClicks >= max_numberOfMouseClicks:
                 return
 
-            previous_timestamp = keypress_time
+            previousTimestamp = timeKeyHeldFor
 
-        elif keypress_time is not None:
-            previous_timestamp = keypress_time
+        elif timeKeyHeldFor is not None:
+            previousTimestamp = timeKeyHeldFor
 
-detect_sandbox()
+
+detectSandbox()
 print("[*] No sandbox detected...")
