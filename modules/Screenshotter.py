319b3297655648b57e7d47cdf7a7a34a471abf9b
Alex Stocks <stocksalex@icloud.com>
2020-09-21 21:02:04 +0100

    Screenshotter.py finished

--- modules/Screenshotter.py
+++ modules/Screenshotter.py
@@ -6,28 +6,28 @@ import win32con
 import win32api
 
 # get handle to entire desktop window
-hdesktop = win32gui.GetDesktopWindow()
+desktopHandle = win32gui.GetDesktopWindow()
 
 width = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
 height = win32api.GetSystemMetrics(win32con.SM_CYVIRTUALSCREEN)
 left = win32api.GetSystemMetrics(win32con.SM_XVIRTUALSCREEN)
 top = win32api.GetSystemMetrics(win32con.SM_YVIRTUALSCREEN)
 
-desktop_dc = win32gui.GetWindowDC(hdesktop)
-img_dc = win32ui.CreateDCFromHandle(desktop_dc)
+desktopDC = win32gui.GetWindowDC(desktopHandle)
+imageDC = win32ui.CreateDCFromHandle(desktopDC)
 
 # create a memory based device context to store image capture
-mem_dc = img_dc.CreateCompatibleDC()
+memoryDC = imageDC.CreateCompatibleDC()
 
 # bitmap object
 screenshot = win32ui.CreateBitmap()
-screenshot.CreateCompatibleBitmap(img_dc, width, height)
-mem_dc.SelectObject(screenshot)
+screenshot.CreateCompatibleBitmap(imageDC, width, height)
+memoryDC.SelectObject(screenshot)
 
 # copy the screen into the memory device context
-mem_dc.BitBlt((0, 0), (width, height), img_dc, (left, top), win32con.SRCCOPY)
+memoryDC.BitBlt((0, 0), (width, height), imageDC, (left, top), win32con.SRCCOPY)
 
-screenshot.SaveBitmapFile(mem_dc, "c:\\Users\\Alex\\Desktop\\screenshot.bmp")
+screenshot.SaveBitmapFile(memoryDC, "c:\\Users\\Alex\\Desktop\\screenshot.bmp")
 
-mem_dc.DeleteDC()
+memoryDC.DeleteDC()
 win32gui.DeleteObject(screenshot.GetHandle())
